dbbbb81416c628401bf22c4ee126ea04db60a933
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-02-04 12:49:54 +0600

    Stupid version

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 'use strict';
+
+var semver = require('semver');
+
+function parseVersion(str) {
+    var re = /\d+\.\d+\.\d+/;
+    var match = re.exec(str);
+    if (!match) { return; }
+    return match[0];
+}
+
+module.exports = function (lhs, rhs) {
+    var diff = {};
+
+    Object.keys(lhs).forEach(function (key) {
+        if (!rhs[key]) { return; }
+
+        var lhv = parseVersion(lhs[key]);
+        var rhv = parseVersion(rhs[key]);
+        if (!lhv || !rhv || lhv === rhv) { return; }
+
+        var lhsv = semver.parse(lhv);
+        var rhsv = semver.parse(rhv);
+
+        diff[key] = {
+            major: rhsv.major - lhsv.major,
+            minor: rhsv.minor - lhsv.minor,
+            patch: rhsv.patch - lhsv.patch
+        };
+    });
+
+    return diff;
+};
--- test/test-diff.js
+++ test/test-diff.js
@@ -1,8 +1,71 @@
 /* global describe, it */
 'use strict';
 
+var dd = require('..'),
+    should = require('should');
+
 describe('dependencies-diff', function () {
-    it('should require without errors', function () {
-        require('..');
+
+    it('should return something', function () {
+        should.exist(dd({}, {}));
+    });
+
+    describe('version formats', function () {
+        var expected = { bem: { minor: 1 } };
+        it('should accept git tags', function () {
+            var before = { 'bem': 'git://github.com/bem/bem-bl.git#0.3.11' };
+            var after = { 'bem': 'git://github.com/bem/bem-bl.git#0.4.11' };
+            dd(before, after).should.containDeep(expected);
+        });
+    });
+
+    describe('version changes', function () {
+        it('should detect patch version change', function () {
+            var before = { 'express': '3.4.1' };
+            var after = { 'express': '3.4.2' };
+
+            var result = dd(before, after);
+            result.should.containDeep({
+                express: {
+                    patch: 1
+                }
+            });
+        });
+
+        it('should detect major version change', function () {
+            var before = { 'express': '2.4.1' };
+            var after = { 'express': '3.4.1' };
+
+            var result = dd(before, after);
+            result.should.containDeep({
+                express: {
+                    major: 1
+                }
+            });
+        });
+
+        it('should detect negative minor version change', function () {
+            var before = { 'express': '2.4.1' };
+            var after = { 'express': '2.3.1' };
+
+            var result = dd(before, after);
+            result.should.containDeep({
+                express: {
+                    minor: -1
+                }
+            });
+        });
+
+        it('should detect negative minor version change', function () {
+            var before = { 'express': '2.4.1' };
+            var after = { 'express': '2.3.1' };
+
+            var result = dd(before, after);
+            result.should.containDeep({
+                express: {
+                    minor: -1
+                }
+            });
+        });
     });
 });
